37f1f09c92659f7b615e37350ec53bc33836b5d8
Sudhan Somasundaram <88369001+Sudhan17@users.noreply.github.com>
2021-08-03 12:53:29 +0530

    Add files via upload

---
+++ Do While Loop/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1, sum = 0;
+    do
+    {
+        sum = sum + i;
+        i += 1;
+    }
+    while(i<=10);
+    cout << sum << endl ;
+    return 0;
+}
