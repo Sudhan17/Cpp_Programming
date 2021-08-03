805ab6c78c9ad9ff1fd48cf6f0b14cce4ed968fb
Sudhan Somasundaram <88369001+Sudhan17@users.noreply.github.com>
2021-08-03 12:52:07 +0530

    Add files via upload

---
+++ Assignment 3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter String: ";
+    getline (cin , str);
+
+
+    cout << endl << "Your string:" << endl << str;
+    return 0;
+}
+
