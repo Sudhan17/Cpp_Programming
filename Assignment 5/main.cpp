4b6a1a01aad5797c79b481077dd36f05ec02c0a2
Sudhan Somasundaram <88369001+Sudhan17@users.noreply.github.com>
2021-08-03 12:52:40 +0530

    Add files via upload

---
+++ Assignment 5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0, sum = 0;
+    for (i = 1; i<=10 ; i++)
+    {
+        sum = sum + i;
+    }
+    cout << sum;
+    return 0;
+}
