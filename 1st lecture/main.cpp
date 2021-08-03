19e269515fb60fdaf19bbacfb9fda99733a327cb
Sudhan Somasundaram <88369001+Sudhan17@users.noreply.github.com>
2021-08-03 12:48:30 +0530

    Add files via upload

---
+++ 1st lecture/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int age;
+
+    cout << " Enter Your Age ";
+    cin >> age;
+
+    cout << "Your Age is : " << age;
+
+
+    return 0;
+}
